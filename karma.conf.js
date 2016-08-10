b3bd9d9faa04a424825c50317169b454a717f230
Mike Thompson <mithomps@redhat.com>
2016-08-10 09:16:59 -0700

    Install karma coverage

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,16 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      reporters: [
+        { type: 'lcov', dir: 'coverage/', subdir: '.' },
+        { type: 'json', dir: 'coverage/', subdir: '.' },
+        { type: 'text-summary' },
+      ]
+
+    },
 
     // web server port
     port: 9876,
